b6b9f77725127df11a5db2186fe19737e8810032
Alexandr <sahsagrom1999@gmail.com>
2020-08-29 17:12:48 +0300

    some shit with unity

---
+++ FireNightCopy/Assets/FireNightCopy/Scripts/JoystickManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class JoystickManager : MonoBehaviour
+{
+    [SerializeField] public FloatingJoystick lookingJoystic;
+    [SerializeField] public FloatingJoystick movingJoystic;
+    [SerializeField] public float mouseSensitivity;
+
+
+    public static JoystickManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+    }
+
+}
